034f0d4bad9a4417dd671ada43bb702236eafac8
MiTo0o <76019114+MiTo0o@users.noreply.github.com>
2021-07-23 10:06:08 -0700

    add env token ;(

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import sys
 from discord.ext import commands
 import discord
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv("DISCORD_TOKEN")
 
 bot = commands.Bot(command_prefix='-')
 
@@ -10,7 +15,7 @@ async def on_ready():
     print("{0.user} is ready to serve.".format(bot))
     await bot.change_presence(activity=discord.Game(name="/help"))
 
-bot.run("ODY1NzE3OTc4NTk5MTI5MTI5.YPIEsg.AidnV2UpDZffgU9nwLz1br4CghE")
+bot.run(TOKEN)
 
 
 print(sys.version)
