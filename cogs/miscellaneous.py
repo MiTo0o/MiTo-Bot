0cda3ef61bec2fb46db731216fbc36baa1d31081
MiTo0o <76019114+MiTo0o@users.noreply.github.com>
2021-07-28 07:11:10 -0700

    try #1 to get audio files to play

--- cogs/miscellaneous.py
+++ cogs/miscellaneous.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+from discord.ext.commands.converter import VoiceChannelConverter
 import youtube_dl
 import time
 import ffmpeg
@@ -25,23 +26,20 @@ class Misc(commands.Cog):
 
     @commands.command(name="play")
     async def play(self, ctx):
-        # Gets voice channel of message author
         voice_channel = ctx.author.voice.channel
-        print(voice_channel)
-        channel = None
         if voice_channel != None:
-            channel = voice_channel.name
             vc = await voice_channel.connect()
-            vc.play(discord.FFmpegPCMAudio(
-                source="\DAM BOI HE THICC.mp3"))
+            vc.play(discord.FFmpegPCMAudio('DAM_BOI_HE_THICC.mp3'))
+
+            # vc.play(discord.FFmpegPCMAudio("DAM_BOI_HE_THICC.mp3"))
             # Sleep while audio is playing.
-            while vc.is_playing():
-                time.sleep(.1)
-            await vc.disconnect()
-        else:
-            await ctx.send(str(ctx.author.name) + "is not in a channel.")
-        # Delete command after the audio is done playing.
-        await ctx.message.delete()
+        #     while vc.is_playing():
+        #         time.sleep(.1)
+        #     await vc.disconnect()
+        # else:
+        #     await ctx.send(str(ctx.author.name) + "is not in a channel.")
+        # # Delete command after the audio is done playing.
+        # await ctx.message.delete()
 
 
 def setup(bot):
