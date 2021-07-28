d4e5263e52eb1a3b2b928a676dfd4c66bd4c8774
MiTo0o <76019114+MiTo0o@users.noreply.github.com>
2021-07-28 11:12:05 -0700

    free real estate

--- cogs/miscellaneous.py
+++ cogs/miscellaneous.py
@@ -1,6 +1,9 @@
 import discord
 from discord.ext import commands
 import time
+from PIL import Image, ImageDraw, ImageFont
+import requests
+from io import BytesIO
 
 
 class Misc(commands.Cog):
@@ -54,8 +57,37 @@ class Misc(commands.Cog):
         await ctx.send(file=discord.File('res/img/animoles/BaNayNay1.jpeg'))
 
     @commands.command(name="sb")
-    async def sb(self, ctx):
-        await ctx.send(file=discord.File('res/img/animoles/BaNayNay1.jpeg'))
+    async def sb(self, ctx, *, user: discord.Member = None):
+        if not user:
+            user = ctx.author
+        response = requests.get(user.avatar_url)
+        url = Image.open(BytesIO(response.content)).convert('RGBA')
+        # if not user:
+        #     user = ctx.author
+        # if user.is_avatar_animated():
+        #     url = user.avatar_url_as(format="gif")
+        # if not user.is_avatar_animated():
+        #     url = user.avatar_url_as(static_format="png")
+
+        gif_file = "res/gifs/salt_bae_loop.gif"
+        salt_bae = Image.open(gif_file, 'r')
+
+        if salt_bae.is_animated:
+            frames = []
+
+            for num in range(salt_bae.n_frames):
+                salt_bae.seek(num)
+                text_img = Image.new('RGBA', (512, 512), (0, 0, 0, 0))
+                text_img.paste(salt_bae, (0, 0))
+                text_img.paste(url, (0, 0), mask=url)
+                frames.append(text_img)
+
+            frames[0].save('out.gif',
+                           save_all=True,
+                           append_images=frames[1:],
+                           duration=100,
+                           loop=0)
+            await ctx.send(file=discord.File('out.gif'))
 
 
 def setup(bot):
