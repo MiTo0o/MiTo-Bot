8381928a0d302d51f0456be203775f19ec14e7a6
MiTo0o <76019114+MiTo0o@users.noreply.github.com>
2021-07-28 00:29:40 -0700

    ye

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
-import sys
-from discord.ext import commands
+
+from discord_slash import cog_ext, SlashContext
+from discord import Embed
 import discord
-import os
+from discord import CategoryChannel, Client, Intents
+from discord.utils import get
+from discord.ext import commands
+from discord_slash import SlashCommand, SlashContext
+from discord_slash.utils.manage_commands import create_option, create_choice
 from dotenv import load_dotenv
+import os
 
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
 
-bot = commands.Bot(command_prefix='-')
+bot = commands.Bot(command_prefix=".")
+# slash = SlashCommand(bot)
 
 
 @bot.event
@@ -15,7 +22,10 @@ async def on_ready():
     print("{0.user} is ready to serve.".format(bot))
     await bot.change_presence(activity=discord.Game(name="/help"))
 
+
+@bot.command()
+async def q5(ctx):
+    await ctx.send("@here QUEUE STARTING IN 5 MINUTES")
+
+
 bot.run(TOKEN)
-
-
-print(sys.version)
